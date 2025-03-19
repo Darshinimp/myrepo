208e84a833e7f87cfc945e86eacb2a896108b4c8
tejas <tejas@TEJAS>
2025-03-19 17:14:22 +0530

    commit 3

---
+++ Myproject1/src/c2tc/Demo.java
@@ -0,0 +1,13 @@
+package c2tc;
+
+public class Demo {
+	public static void main(String args[]) {
+		program ob = new program();
+		ob.getId();
+		ob.setId(2);
+		ob.setcollegeName("mvj");
+		ob.setName("Darshini");
+		ob.
+	}
+
+}
--- Myproject1/src/c2tc/program.java
+++ Myproject1/src/c2tc/program.java
@@ -1,5 +1,33 @@
 package c2tc;
 
 public class program {
+	private int id;
+	String name;
+	protected int marks;
+	public String collegeName;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMarks() {
+		return marks;
+	}
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+	public String getCollegeName() {
+		return collegeName;
+	}
+	public void setCollegeName(String collegeName) {
+		this.collegeName = collegeName;
+	}
 
 }
