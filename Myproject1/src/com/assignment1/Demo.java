13475adb570b8c41e9aab9ccb08d024f48f9756b
tejas <tejas@TEJAS>
2025-03-27 19:48:34 +0530

    commit3

---
+++ Myproject1/src/com/assignment1/Demo.java
@@ -0,0 +1,14 @@
+package com.assignment1;
+
+public class Demo {
+	 public static void main(String[] args) {
+	        // Creating Student object to invoke the default constructor
+	        Student student = new Student();
+
+	        // Creating Commission object and using its methods
+	        Commission employee = new Commission();
+	        employee.acceptDetails();
+	        employee.calculateCommission();
+	    }
+
+}
