00f3a1187934e7fd2ca21621ef2d7736fa4832bf
tejas <tejas@TEJAS>
2025-03-27 21:52:38 +0530

    commit4

---
+++ Myproject1/src/com/ques2/circle/Demo2.java
@@ -0,0 +1,17 @@
+package com.ques2.circle;
+
+public class Demo2 {
+	public static void main(String[] args) {
+       
+        Student student = new Student();
+       
+
+        Commission employee = new Commission();
+        employee.acceptDetails();
+        employee.calculateCommission();
+        Student circle = new Student();
+        circle.getInput();
+        circle.calcArea();
+    }
+
+}
